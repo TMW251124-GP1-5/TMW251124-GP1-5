f5211bf75f046b8443814087b5f5ec7d6613f894
owusuvictorjnr <owusuvictor80@gmail.com>
2024-12-02 13:47:00 +0000

    add comments

--- project-9-web-app/server/routes/api.js
+++ project-9-web-app/server/routes/api.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { orderHandler } from "../firebaseConnection/order.js";
 
+// express router instance to handle the routes
 const router = express.Router();
 
-
+// route to handle the order request
 router.post("/order", orderHandler);
 
 export default router;
